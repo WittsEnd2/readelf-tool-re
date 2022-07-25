81bdbd2e485a307b37518f1b657010a6c5e0bdea
WittsEnd2 <contact@ragnarsecurity.com>
2022-07-25 13:08:59 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if () 
+    for arg in args 
+    {
+        if arg.find("-") != None {
+            println!("{:?}", arg);
+        }
+        println!("{:?}", arg);
+    }
+}
+
